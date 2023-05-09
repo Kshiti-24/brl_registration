251626baae143f80d888cf81561281119360e47f
Kshitiz <kshitizagarwa2405@gmail.com>
2023-05-10 00:13:15 +0530

    Final

--- lib/widgets/text_form_field.dart
+++ lib/widgets/text_form_field.dart
@@ -3,24 +3,29 @@ import 'package:flutter/material.dart';
 Widget textFormFieldWidget(
     {required TextEditingController controller,
     required String hintText,
-    required IconData icon,
+    IconData? icon,
+    required String img,
     required TextInputType keyboardType,
     required String? Function(String?) validator}) {
   return TextFormField(
     controller: controller,
     keyboardType: keyboardType,
     decoration: InputDecoration(
-      hintText: hintText,
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      suffixIcon: Icon(
-        icon,
-        size: 20,
-        color: Colors.black,
-      ),
-    ),
+        hintText: hintText,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        contentPadding: EdgeInsets.symmetric(vertical: 14, horizontal: 10),
+        suffixIcon: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: SizedBox(
+            width: 5,
+            height: 10,
+            child: Image.asset(img.toString()),
+          ),
+        )),
     autovalidateMode: AutovalidateMode.onUserInteraction,
     validator: validator,
   );
+  print(img);
 }
